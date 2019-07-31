d771fb3d39bb0b0cde5d6ea0d5d5ef1de8e67172
jason hsu <appletiger0704@gmail.com>
2019-07-31 16:02:42 +0800

    first commit

---
+++ products.py
@@ -0,0 +1,16 @@
+products = []
+while True :
+	name = input('請輸入商品名稱:')
+	if name == 'q' :
+		break
+	price = input('請輸入商品價格:')
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p) #二維清單
+#products.append([name, price])
+print(products)
+
+with open('products.csv','w') as f :
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
